903fbb77d791f9e977507f6db42c0c6247da84de
Ji Wang <44724621+KPLJ@users.noreply.github.com>
2021-09-10 12:50:23 -0400

    create PhotoMania

---
+++ PhotoMania/PhotoMania/ContentView.swift
@@ -0,0 +1,83 @@
+//
+//  ContentView.swift
+//  PhotoMania
+//
+//  Created by Ji Wang on 9/10/21.
+//
+
+import SwiftUI
+
+class ViewModel: ObservableObject {
+    @Published var image: Image?
+    
+    func fetchNewImage() {
+        guard let url = URL(string: "https://random.imagecdn.app/500/500") else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, _ in
+            guard let data = data else { return }
+            
+            DispatchQueue.main.async {
+                guard let uiImage = UIImage(data: data) else { return }
+                self.image = Image(uiImage: uiImage)
+            }
+        }
+        task.resume() // resume() starts this task
+    }
+}
+
+struct ContentView: View {
+    @StateObject var viewModel = ViewModel()
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                
+                Spacer()
+                
+                if let image = viewModel.image {
+                    ZStack {
+                        image
+                            .resizable()
+                            .foregroundColor(Color.pink)
+                            .frame(width: 200, height: 200)
+                            .padding()
+                    }
+                    .frame(width: UIScreen.main.bounds.width / 1.2, height: UIScreen.main.bounds.width / 1.2)
+                    .background(Color.green)
+                    .cornerRadius(8)
+                } else { // if no image from viewModel
+                    Image(systemName: "photo")
+                        .resizable()
+                        .foregroundColor(Color.pink)
+                        .frame(width: 200, height: 200)
+                        .padding()
+                }
+                
+                Spacer()
+                
+                Button(action: {
+                    viewModel.fetchNewImage()
+                }, label: {
+                    Text("New Image")
+                        .bold()
+                        .frame(width: 250, height: 55)
+                        .foregroundColor(Color.white)
+                        .background(Color.blue)
+                        .cornerRadius(8)
+                        .padding()
+                })
+            }
+            .navigationTitle("Photo Mania")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+        }
+    }
+}
---
+++ PhotoMania/PhotoMania/PhotoManiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoManiaApp.swift
+//  PhotoMania
+//
+//  Created by Ji Wang on 9/10/21.
+//
+
+import SwiftUI
+
+@main
+struct PhotoManiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
