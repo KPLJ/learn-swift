f8608f362fb88b557c5f66caedccbbaeaf6c8ca0
Ji Wang <44724621+KPLJ@users.noreply.github.com>
2021-09-10 13:20:35 -0400

    create SwiftUI-UserLocation

---
+++ SwiftUI-UserLocation/SwiftUI-UserLocation/SwiftUI_UserLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_UserLocationApp.swift
+//  SwiftUI-UserLocation
+//
+//  Created by Ji Wang on 9/10/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_UserLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
